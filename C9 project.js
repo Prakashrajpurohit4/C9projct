f85ec1904091ea4a8e2146b4cb85d9de8d8d3bb4
Prakashrajpurohit4 <94972776+Prakashrajpurohit4@users.noreply.github.com>
2021-11-27 15:01:45 +0530

    Add files via upload

---
+++ C9 project.js
@@ -0,0 +1,31 @@
+
+function setup() {
+  createCanvas(400,400);
+var ufo=createSprite(250,350,20,20);
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+function draw()
+{
+  background("red");
+drawSprites();
+}
+
+
+
+
